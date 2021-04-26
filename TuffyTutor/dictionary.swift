ac7511915c54ee6112600a98ddee4dc21da5ffd6
Massimo Villaeal <massimovillareal@csu.fullerton.edu>
2021-04-25 18:58:14 -0700

    new

--- TuffyTutor/dictionary.swift
+++ TuffyTutor/dictionary.swift
@@ -7,19 +7,21 @@
 
 import Foundation
 
-struct tutorAndSubject{
+/*struct tutorAndSubject{
     
    var arrayofTutors = [Array<Any>]()
-    var arrayofSubjects = [String]()
+    var arrayofSubjects = [String()]
     
    
     /*var tutorSubject = [Array: String]()     //this creates a dictionary, key: tutors, value: subject !!!!!!!!!! array as value */
     
     var subjectTutor = [String:Array<Any>]()     //this creates a dictionary, key: subject , value: tutors !!!!!!!!!! array as value
     
-    init(){
+    init(subject: String, tutor: Array<Any>){
         //tutorSubject = [:] // makes empty. safety
-        subjectTutor = [:]// makes empty. safety
+        //subjectTutor[subject]// makes empty. safety
+       // arrayofSubjects = tutor
+        self.subjectTutor = subject
     }
     
    /* mutating func addTS(tutor: String, subject: String){
@@ -52,7 +54,36 @@ struct tutorAndSubject{
     
     
     
+    
+    
  
+    
+} */
+
+
+
+// a regular dictionary with subject as key, and tutor as a value
+
+// if we want to search we just use for loop
+
+
+struct dictionary{
+    
+    var SubjectTutor: [String: Array<Tutor>]
+    
+    init(){
+        
+        SubjectTutor = [String: Array<Tutor>]()
+    }
+    
+    mutating func add(subject: String, tutor: Tutor){
+        if SubjectTutor[subject] == nil{
+            SubjectTutor[subject] = [tutor]
+        }
+        else{
+            SubjectTutor[subject]!.append(tutor)
+        }
     
 }
 
+}
