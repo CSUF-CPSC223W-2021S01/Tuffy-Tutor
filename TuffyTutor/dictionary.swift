4c892dd0beb517563b19136cd84b59f3bacdc2f2
Massimo Villaeal <massimovillareal@csu.fullerton.edu>
2021-04-25 17:31:45 -0700

    fix dictionary

--- TuffyTutor/dictionary.swift
+++ TuffyTutor/dictionary.swift
@@ -9,18 +9,20 @@ import Foundation
 
 struct tutorAndSubject{
     
-   var arrayofTutors = [String]()
+   var arrayofTutors = [Array<Any>]()
     var arrayofSubjects = [String]()
     
-    var tutorSubject = [String:Array<Any>]()     //this creates a dictionary, key: tutors, value: subject !!!!!!!!!! array as value
+   
+    /*var tutorSubject = [Array: String]()     //this creates a dictionary, key: tutors, value: subject !!!!!!!!!! array as value */
+    
     var subjectTutor = [String:Array<Any>]()     //this creates a dictionary, key: subject , value: tutors !!!!!!!!!! array as value
     
     init(){
-        tutorSubject = [:] // makes empty. safety
+        //tutorSubject = [:] // makes empty. safety
         subjectTutor = [:]// makes empty. safety
     }
     
-    mutating func addTS(tutor: String, subject: String){
+   /* mutating func addTS(tutor: String, subject: String){
         if tutorSubject[tutor] == nil{      //this checks if the the tutor does not exist
             arrayofSubjects.append(subject) // adds subject to array
             tutorSubject[tutor] = arrayofSubjects   //if it does not exist, it adds it with the corresponding subjects
@@ -32,8 +34,9 @@ struct tutorAndSubject{
                 print("Subject Added to Tutor")      // simply prints out that tutor already exist
             }
         }
-    }
-    mutating func addST(subject: String, tutor: String){
+    }*/
+ 
+    mutating func addST(subject: String, tutor: Array<Any>){
         if subjectTutor[subject] == nil{      //this checks if the the subject does not exist
             arrayofTutors.append(tutor)
             subjectTutor[subject] = arrayofTutors   //if it does not exist, it adds it with the corresponding tutors
