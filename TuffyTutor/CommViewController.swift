81f88a6af86207f6d84f61ba5e0d9e4b3c4abf0c
Rafael Lopez <rafaell3724@csu.fullerton.edu>
2021-05-06 15:05:21 -0700

    ver2

--- TuffyTutor/CommViewController.swift
+++ TuffyTutor/CommViewController.swift
@@ -82,13 +82,15 @@ class CommViewController: UIViewController {
 
     }
 
-    /*
+    */
      // MARK: - Navigation
-
+/*
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
          // Get the new view controller using segue.destination.
          // Pass the selected object to the new view controller.
      }
      */
+ 
+ }
 }
