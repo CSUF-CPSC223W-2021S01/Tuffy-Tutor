4a197639ff6da17896cc484a7aaf0035f026364a
ej1069849e <54593374+ej1069849e@users.noreply.github.com>
2021-05-06 18:30:12 -0700

    Add files via upload

---
+++ Tuffy-Tutor/TuffyTutor/Subject.swift
@@ -0,0 +1,24 @@
+//
+//  Subject.swift
+//  TuffyTutor
+//
+//  Created by Evelyn Johnson on 5/4/21.
+//
+
+import Foundation
+
+struct Subject {
+    var ClassSub = String()
+    
+    init(ClassSub: String) {
+        self.ClassSub = ClassSub
+    }
+
+    func getSubjectString() -> String {
+        return "Subject: " + ClassSub
+    }
+}
+
+
+// test comment fdmsakfhjdaifojaif
+// test comment 2
