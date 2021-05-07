71aefd96e0db80a0300baac9dd117555f7c0177d
Evelyn Johnson <evelyn.irj@gmail.com>
2021-05-06 23:03:49 -0700

    Evelyn's new branch

--- TuffyTutor-/ViewController.swift
+++ TuffyTutor-/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -25,5 +24,6 @@ class ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
 
 }
