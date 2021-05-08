1bdbb41a5d4095fcdcd60c1516db44ba0c968a60
Evelyn Johnson <evelyn.irj@gmail.com>
2021-05-07 18:44:30 -0700

    updated bookaTutor.swift file

--- TuffyTutor-/BookaTutorViewController.swift
+++ TuffyTutor-/BookaTutorViewController.swift
@@ -36,8 +36,6 @@ class BookaTutorViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var inputEndtime: UITextField!
     
-    var bookTutor : bookaTutor? // this is where I initilize user input into object
-    var booking : Book?         // encode/decode located here
     
     let userTutor = UserDefaults()
     let userSubj = UserDefaults()
@@ -72,10 +70,6 @@ class BookaTutorViewController: UIViewController, UITextFieldDelegate {
             EndTimeLabel.text = "last saved End time was " + value5 + ". What End time would you like now?"
         }
         
-        bookTutor = bookaTutor(Tutor: TutorNameLabel.text!, Subject: SubjectNameLabel.text! , day: DayNameLabel.text!, StartTime: StartTimeLabel.text!, EndTime: EndTimeLabel.text!)
-        
-        booking = Book()
-        //
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -89,7 +83,6 @@ class BookaTutorViewController: UIViewController, UITextFieldDelegate {
        // inputTutor.resignFirstResponder() // tells system to return keyboard
         return true
     }
-    
     
     
     
